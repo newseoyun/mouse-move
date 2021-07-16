16b494bda33e403f448995f07fddebd044c540b3
seoyun <newseoyun@gmail.com>
2021-07-16 21:57:35 +0900

    .

--- com/seoyun/MouseMove.java
+++ com/seoyun/MouseMove.java
@@ -1,9 +1,30 @@
 package com.seoyun;
 
+import java.awt.*;
+
 public class MouseMove {
-    
+
     public static void main(String[] args) {
-        
+
+        new WindowBox();
+
+        try {
+            while (true) {
+                Robot robot = new Robot();
+
+                robot.mouseMove(100, 100);
+                robot.delay(5000);
+                robot.mouseMove(300, 100);
+                robot.delay(5000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
-    
+
+
+
+
 }
--- com/seoyun/WindowBox.java
+++ com/seoyun/WindowBox.java
@@ -1,10 +1,54 @@
 package com.seoyun;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class WindowBox extends JFrame {
-    
-    
 
+    JLabel jl = new JLabel();
+    JLabel jl2 = new JLabel();
+    JPanel jPanel = new JPanel();
+
+    public WindowBox() {
+
+        super("move move");
+
+        setSize(400, 200);
+
+        myMouseListener listener = new myMouseListener();
+
+        jl.setText("이것은 그저 마우스를 주기적으로 움직이는 프로그램이다.");
+        jl.setFont(new Font("바탕체", Font.PLAIN, 14));
+        addMouseMotionListener(listener);
+        jPanel.add(jl);
+        jPanel.add(jl2);
+
+        add(jPanel);
+
+        setLocation(0, 0);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    class myMouseListener implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            LocalTime localTime = LocalTime.now();
+            DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String time = localTime.format(f);
+            jl2.setText("[" + time + "] " + e.getX() + "." + e.getY() + " 으로 이동했음");
+        }
+    }
 
 }
